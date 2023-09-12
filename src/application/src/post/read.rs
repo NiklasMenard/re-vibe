7bae2edd720f3d2ee60c51bed89652bf971f9863
nmenard <niklas.menard@gmail.com>
2023-09-12 14:10:45 +0300

    application method updates, user delete WIP

--- src/application/src/post/read.rs
+++ src/application/src/post/read.rs
@@ -4,6 +4,25 @@ use infrastructure::database::connection::establish_connection;
 use rocket::response::status::NotFound;
 use shared::response_models::{Response, ResponseBody};
 
+pub fn list_posts() -> Vec<Post> {
+    let connect = &mut establish_connection();
+
+    match posts::table
+        .select(posts::all_columns)
+        .load::<Post>(connect)
+    {
+        Ok(mut posts) => {
+            posts.sort();
+            posts
+        }
+        Err(err) => match err {
+            _ => {
+                panic!("Database error - {}", err);
+            }
+        },
+    }
+}
+
 pub fn list_post(post_id: i32) -> Result<Post, NotFound<String>> {
     let connect = &mut establish_connection();
 
@@ -19,25 +38,6 @@ pub fn list_post(post_id: i32) -> Result<Post, NotFound<String>> {
                 };
                 return Err(NotFound(serde_json::to_string(&response).unwrap()));
             }
-            _ => {
-                panic!("Database error - {}", err);
-            }
-        },
-    }
-}
-
-pub fn list_posts() -> Vec<Post> {
-    let connect = &mut establish_connection();
-
-    match posts::table
-        .select(posts::all_columns)
-        .load::<Post>(connect)
-    {
-        Ok(mut posts) => {
-            posts.sort();
-            posts
-        }
-        Err(err) => match err {
             _ => {
                 panic!("Database error - {}", err);
             }
--- src/application/src/user/delete.rs
+++ src/application/src/user/delete.rs
@@ -2,12 +2,28 @@ use diesel::prelude::*;
 use diesel::QueryDsl;
 use domain::schema::users;
 use infrastructure::database::connection::establish_connection;
+use rocket::response::status::NotFound;
+use shared::response_models::{Response, ResponseBody};
 use uuid::Uuid;
 
-pub fn delete_user(id: Uuid) -> bool {
+pub fn delete_user(id: Uuid) -> Result<(), NotFound<String>> {
     let connection = &mut establish_connection();
 
-    diesel::delete(users::table.find(id))
-        .execute(connection)
-        .is_ok()
+    match diesel::delete(users::table.find(id)).execute(connection) {
+        Ok(_) => Ok(()),
+        Err(err) => match err {
+            diesel::result::Error::NotFound => {
+                let response = Response {
+                    body: ResponseBody::Message(format!(
+                        "Error deleting user with id {:?} - {:?}",
+                        id, err
+                    )),
+                };
+                return Err(NotFound(serde_json::to_string(&response).unwrap()));
+            }
+            _ => {
+                panic!("Database error - {}", err);
+            }
+        },
+    }
 }
