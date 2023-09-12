0cc88a71dc3e22d0987383dd143ded77948f2af2
nmenard <niklas.menard@gmail.com>
2023-09-12 14:09:54 +0300

    route protection and other updates

--- src/api/src/post_handler.rs
+++ src/api/src/post_handler.rs
@@ -1,61 +1,86 @@
 use application::post::{create, delete, read, update};
 use domain::models::NewPost;
 use infrastructure::auth::ApiKey;
-use rocket::response::status::{Created, NotFound};
+
+use rocket::http::Status;
+use rocket::response::status::Created;
 
 use rocket::serde::json::Json;
 use rocket::{delete, get, post, put};
 use shared::response_models::{Response, ResponseBody};
 
 #[get("/posts")]
-pub fn list_posts_handler(__key: ApiKey) -> String {
-    let posts = read::list_posts();
+pub fn list_posts_handler(key: ApiKey) -> Result<String, Status> {
+    match ApiKey::verify_user_role(&key.role) {
+        true => {
+            let posts = read::list_posts();
 
-    let response = Response {
-        body: ResponseBody::Posts(posts),
-    };
+            let response = Response {
+                body: ResponseBody::Posts(posts),
+            };
 
-    serde_json::to_string(&response).unwrap()
+            Ok(serde_json::to_string(&response).unwrap())
+        }
+        _ => return Err(Status::Unauthorized),
+    }
 }
 
 #[get("/posts/<post_id>")]
-pub fn list_post_handler(_key: ApiKey, post_id: i32) -> Result<String, NotFound<String>> {
-    let post = read::list_post(post_id)?;
+pub fn list_post_handler(key: ApiKey, post_id: i32) -> Result<String, Status> {
+    match ApiKey::verify_user_role(&key.role) {
+        true => {
+            let found_post = read::list_post(post_id).unwrap();
 
-    let response = Response {
-        body: ResponseBody::Post(post),
-    };
+            let response = Response {
+                body: ResponseBody::Post(found_post),
+            };
 
-    Ok(serde_json::to_string(&response).unwrap())
+            Ok(serde_json::to_string(&response).unwrap())
+        }
+        _ => return Err(Status::Unauthorized),
+    }
 }
 
 #[put("/posts/<post_id>", format = "application/json", data = "<post>")]
 pub fn update_post_handler(
-    _key: ApiKey,
+    key: ApiKey,
     post_id: i32,
     post: Json<NewPost>,
-) -> Result<String, NotFound<String>> {
-    let post = update::update_post(post_id, post)?;
+) -> Result<String, Status> {
+    match ApiKey::verify_user_role(&key.role) {
+        true => {
+            let post = update::update_post(post_id, post).unwrap();
 
-    let response = Response {
-        body: ResponseBody::Post(post),
-    };
+            let response = Response {
+                body: ResponseBody::Post(post),
+            };
 
-    Ok(serde_json::to_string(&response).unwrap())
+            Ok(serde_json::to_string(&response).unwrap())
+        }
+        _ => return Err(Status::Unauthorized),
+    }
 }
 
 #[post("/posts", format = "application/json", data = "<post>")]
-pub fn create_post_handler(_key: ApiKey, post: Json<NewPost>) -> Created<String> {
-    create::create_post(post)
+pub fn create_post_handler(key: ApiKey, post: Json<NewPost>) -> Result<Created<String>, Status> {
+    match ApiKey::verify_user_role(&key.role) {
+        true => Ok(create::create_post(post)),
+        _ => return Err(Status::Unauthorized),
+    }
 }
 
 #[delete("/posts/<id>")]
-pub fn delete_post_handler(_key: ApiKey, id: i32) -> Result<String, NotFound<String>> {
-    let posts = delete::delete_post(id)?;
+pub fn delete_post_handler(key: ApiKey, id: i32) -> Result<String, Status> {
+    match ApiKey::verify_user_role(&key.role) {
+        true => {
+            let posts = delete::delete_post(id).unwrap();
 
-    let response = Response {
-        body: ResponseBody::Posts(posts),
-    };
+            let response = Response {
+                body: ResponseBody::Posts(posts),
+            };
 
-    Ok(serde_json::to_string(&response).unwrap())
+            Ok(serde_json::to_string(&response).unwrap())
+        }
+        _ => return Err(Status::Unauthorized),
+    }
 }
