e3c2910e00945a1128a3fff2aea6a1e973a6fae9
nmenard <niklas.menard@gmail.com>
2024-09-17 20:32:21 +0300

    shadcn and tailwing configs

--- UI/vite.config.ts
+++ UI/vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     sourcemap: true,
   },
   plugins: [react()],
-    server: {
+  server: {
     port: 3000,
   },
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: path.resolve(__dirname, './postcss.config.js'),
+  },
+});
